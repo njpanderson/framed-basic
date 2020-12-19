c60c67014fa58e05d76276eaf269645920df516a
Neil Anderson <njp.anderson@icloud.com>
2020-12-19 16:15:20 +0000

    Update index to support up to date config

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 const rollup = require('rollup');
-const rollupConfig = require('./rollup.config.js');
+const config = require('./rollup.config.js');
 
 module.exports = {
 	root: __dirname,
 	builder: (options, outputFile) => {
 		// Return the promise
 		return new Promise((resolve, reject) => {
-			let jsFilename = 'bundle.js',
-				config, compiler;
+			let jsFilename = 'bundle.js';
 
-			config = rollupConfig(options);
-
-			config.outputOptions = Object.assign({}, config.outputOptions, {
+			config.output = {
+				...config.outputOptions,
 				file: outputFile
-			});
+			};
 
-			rollup.rollup(config.inputOptions)
+			rollup.rollup(config.input)
 				.then((bundle) => bundle.write(config.outputOptions))
 				.then(() => {
 					resolve({
